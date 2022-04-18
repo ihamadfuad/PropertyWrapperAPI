2274a71fe14347bf4494953520ebf6b517867104
Hamad Fuad <hamad.ali@procco.com>
2022-04-18 09:47:27 +0300

    Initial Commit

---
+++ Sources/PropertyWrapperAPI/PropertyWrapperAPI.swift
@@ -0,0 +1,6 @@
+public struct PropertyWrapperAPI {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/PropertyWrapperAPITests/PropertyWrapperAPITests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import PropertyWrapperAPI
+
+final class PropertyWrapperAPITests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(PropertyWrapperAPI().text, "Hello, World!")
+    }
+}
